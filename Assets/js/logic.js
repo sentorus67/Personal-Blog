43efec305cf6e9c286a25045e2a95aef8fb83314
Jemeric <tyuity14450@hotmail.com>
2024-03-24 13:29:37 -0400

    added a light and dark data-mode to the logic.js(and therefore index.html) file.

--- Assets/js/logic.js
+++ Assets/js/logic.js
@@ -1,20 +1,39 @@
 
-let currentPage=document.getElementById('inputForm');
-let switchButton= document.getElementById('switchPage');
+const currentPage=document.getElementById('inputForm');
+const switchButton= document.getElementById('switchPage');
+const  lightSwitch= document.getElementById('lightSwitch');
+const colorMode= document.querySelectorAll('[data-mode]');
 
- function changeLocation(){
+
+
+function changeLocation(){
   
     if (currentPage.getAttribute('id')=="inputForm")
     {
-        //alert("The page attempted to change.");
        window.location.href = "blog.html";
        return;
     }
     else
-    {   //alert("the id is incorrect");
+    {   
         window.location.href="index.html";
         return;
     }
 }
 
+function lightAndDark(x){
+    if (x.dataset.mode=="light"){
+        x.dataset.mode="dark";
+    }
+    else{
+        x.dataset.mode="light";
+    }
+}
+
+lightSwitch.addEventListener("click",function(){
+for (let index = 0; index < colorMode.length; index++) {
+    
+    lightAndDark(colorMode[index]);
+}
+
+});
 switchButton.addEventListener("click",changeLocation);
